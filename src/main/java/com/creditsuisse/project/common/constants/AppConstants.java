ffa7d10053f6987edf8946dc336b04f80519453a
Elavarasan Rajednran <elavarasan821090@gmail.com>
2021-08-01 19:57:19 +0100

    feature - add implementation for log parser

---
+++ src/main/java/com/creditsuisse/project/common/constants/AppConstants.java
@@ -0,0 +1,18 @@
+package com.creditsuisse.project.common.constants;
+
+public class AppConstants {
+    private AppConstants() {
+        // Prevent Instantiation
+    }
+
+    //Exception Messages
+    public static final String INVALID_PARAMS_ERROR_MESSAGE = "Please provide the correct log file Path";
+    public static final String FILE_NOT_FOUND_ERROR_MESSAGE = "File not found at the provided location. " +
+            "Please check the location and try again";
+    public static final String EVENT_CONVERSION_ERROR_MESSAGE = "There was  error mapping the event log line from the file: {}";
+
+
+    // SQL Constants
+    public static final String CREATE_TABLE_STATEMENT = "CREATE TABLE IF NOT EXISTS event (id VARCHAR(20)," +
+            " duration INTEGER, type VARCHAR(50), host VARCHAR(50), alert BOOLEAN)";
+}
